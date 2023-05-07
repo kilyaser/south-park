a01e44eeede4fe87e0a6805470067cc5dbdaf785
arcadag <piknik0711@yandex.ru>
2023-05-07 18:36:17 +0300

    implemented a SouthParkController for test deployment

---
+++ src/main/java/com/aston/southpark/controller/SouthParkController.java
@@ -0,0 +1,15 @@
+package com.aston.southpark.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class SouthParkController {
+    @GetMapping
+    public String getSouthParkHello(){
+        return "This is South Park project";
+    }
+}
