3bafd9f05604b7f938900ef407723c1ba8d781e6
Your Namevitalii_m <vprohorof@gmail.com>
2023-05-15 17:41:46 +0700

    create liquibase

---
+++ src/main/java/com/aston/southpark/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.aston.southpark.dao;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "user", schema = "public")
+public class UserDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    }
+
