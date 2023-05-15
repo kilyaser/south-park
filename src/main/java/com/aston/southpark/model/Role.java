f9a16e233e50b2c39aca7479177e06ac5ec3cb84
arcadag <piknik0711@yandex.ru>
2023-05-15 23:28:03 +0300

    implemented a User and Role entities

---
+++ src/main/java/com/aston/southpark/model/Role.java
@@ -0,0 +1,17 @@
+package com.aston.southpark.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "role")
+    private String role;
+}
