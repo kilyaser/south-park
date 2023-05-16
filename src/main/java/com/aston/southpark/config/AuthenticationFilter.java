8a796db4ac3e55a995dd55447d5362f1d4be4fdb
arcadag <piknik0711@yandex.ru>
2023-05-17 02:40:16 +0300

    implemented spring security

--- src/main/java/com/aston/southpark/config/AuthenticationFilter.java
+++ src/main/java/com/aston/southpark/config/AuthenticationFilter.java
@@ -2,22 +2,26 @@ package com.aston.southpark.config;
 
 import com.aston.southpark.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
-
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     @Override
@@ -26,6 +30,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         if (jwt != null) {
             String user = jwtService.getAuthUser(request);
             if (Objects.nonNull(user) && Objects.isNull(SecurityContextHolder.getContext().getAuthentication())) {
+                log.info("roles: {}", jwtService.getAuthority(jwt));
                 Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, jwtService.getAuthority(jwt));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
