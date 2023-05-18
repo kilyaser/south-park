746c9dd40361ffe2264e2037b3b4d1a336b5f2fc
noir1915 <astronaut95@list.ru>
2023-05-18 08:50:30 +0300

    changed the class Order

--- src/main/java/com/aston/southpark/model/Order.java
+++ src/main/java/com/aston/southpark/model/Order.java
@@ -4,7 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -20,11 +26,16 @@ public class Order {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "customer", nullable = false)
     @ManyToOne
     private Customer customer;
+    @Column(name = "created", nullable = false)
     private LocalDateTime created;
+    @Column(name = "modified", nullable = false)
     private LocalDateTime modified;
+    @Column(name = "total_cost", nullable = false)
     private BigDecimal totalCost;
+    @Column(name = "completion", nullable = false)
     private LocalDateTime completion;
 
     @Column(name = "order_title", nullable = false)
