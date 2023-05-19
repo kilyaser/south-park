1868736875665ae9c3ec49ca79cf86356b6955a7
Dmitrii Berlov <astronaut95@list.ru>
2023-05-19 12:54:08 +0400

    create and update entity-classes

---
+++ src/main/java/com/aston/southpark/model/OrderItem.java
@@ -0,0 +1,44 @@
+
+
+
+package com.aston.southpark.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "order_item", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "price_per_product")
+    private BigDecimal pricePerProduct;
+}
--- src/main/java/com/aston/southpark/model/Role.java
+++ src/main/java/com/aston/southpark/model/Role.java
@@ -9,16 +9,20 @@ import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Column;
 import javax.persistence.GenerationType;
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @Table(name = "role")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "role_name")
     private String roleName;
 }
+
