62a740aab12cf84b0906e2e6803e0c5df17fa494
arcadag <piknik0711@yandex.ru>
2023-05-23 20:20:38 +0300

    set relationship between Technologist and Product

--- src/main/java/com/aston/southpark/model/Technologist.java
+++ src/main/java/com/aston/southpark/model/Technologist.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "technologist")
@@ -21,5 +22,7 @@ public class Technologist {
     private String name;
     @Column(name = "email")
     private String email;
+    @OneToMany(mappedBy = "technologist")
+    private List<Product> products;
 
 }
