4d3d026b512198a66e96f6785df005dac49f1ccb
noir1915 <astronaut95@list.ru>
2023-05-25 13:44:21 +0300

    без тестов

--- src/main/java/com/aston/southpark/service/CustomerService.java
+++ src/main/java/com/aston/southpark/service/CustomerService.java
@@ -43,27 +43,6 @@ public class CustomerService {
     public void update(CustomerDto customerDto) {
         Customer customer = customerRepository.findById(customerDto.getId())
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Customer with id = %d not found", customerDto.getId())));
-        customer.setOrders((List<Order>) orderConverter.toEntity(customerDto.getOrderDto()));
-        customer.setName(customerDto.getName());
-        customer.setEmail(customerDto.getEmail());
-        customer.setPhone(customerDto.getPhone());
-    }
-
-    public Customer create(CustomerDto customerDto) {
-        return customerRepository.save(customerConverter.toEntity(customerDto));
-    }
-
-    public void remove(Long id) {
-        customerRepository.delete(customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Customer with id = %d not found", id))));
-    }
-
-    public List<Customer> getAll() {
-        return customerRepository.findAll();
-    }
-
-    @Transactional
-    public void update(CustomerDto customerDto) {
-        Customer customer = customerRepository.findById(customerDto.getId()).orElseThrow(() -> new ResourceNotFoundException(String.format("Customer with id = %d not found", customerDto.getId())));
         customer.setOrders((List<Order>) orderConverter.mapToOrderEntity(customerDto.getOrderDto()));
         customer.setName(customerDto.getName());
         customer.setEmail(customerDto.getEmail());
