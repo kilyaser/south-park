baee7f51308eb176163201376beed5c847ff3cec
vitaly.m <vprohorof@gmail.com>
2023-06-04 18:05:14 +0700

    entity Customer & Material добавил описание api

--- src/main/java/com/aston/southpark/controller/MaterialController.java
+++ src/main/java/com/aston/southpark/controller/MaterialController.java
@@ -2,6 +2,10 @@ package com.aston.southpark.controller;
 
 import com.aston.southpark.dto.MaterialDto;
 import com.aston.southpark.service.MaterialService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +18,41 @@ public class MaterialController {
     private final MaterialService materialService;
 
     @GetMapping("/{material}")
+    @Operation(summary = "Получение информации о материалах")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "400", description = "Некорректный запрос", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "401", description = "Авторизуйтесь для операции", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "403", description = "доступ к запрошенному ресурсу запрещен", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "500", description = "Сервер занят, напишите позже", content = @Content(mediaType = "")),
+    })
     public ResponseEntity<?> getMaterialById(@PathVariable Long id) {
         return ResponseEntity.ok(materialService.getById(id));
     }
 
     @PostMapping("/{material}")
+    @Operation(summary = "Добавление/обновление информации о материалах")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное добавление/обновлние", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "400", description = "Некорректный запрос", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "401", description = "Авторизуйтесь для операции", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "403", description = "доступ к запрошенному ресурсу запрещен", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "500", description = "Сервер занят, напишите позже", content = @Content(mediaType = "")),
+    })
     public ResponseEntity<?> createMaterial(@RequestBody MaterialDto materialDto) {
         materialService.create(materialDto);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление информации о материалах")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное удаление", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "400", description = "Некорректный запрос", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "401", description = "Авторизуйтесь для операции", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "403", description = "доступ к запрошенному ресурсу запрещен", content = @Content(mediaType = "")),
+            @ApiResponse(responseCode = "500", description = "Сервер занят, напишите позже", content = @Content(mediaType = "")),
+    })
     public void deleteMaterial(@PathVariable Long id) {
         materialService.remove(id);
     }
---
+++ src/main/resources/db/data/07-material-test.sql
@@ -0,0 +1,4 @@
+insert into public.material(id, type)
+values (2, 'палка'),
+       (3, 'оно'),
+       (4, 'пластик');
